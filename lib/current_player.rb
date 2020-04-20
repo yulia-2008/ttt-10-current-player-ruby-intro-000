63ad6c5ddc13eb9d92242199b352959521b4fcc7
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-20 15:27:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,4 @@
 def turn_count(board)
-  board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  count=0 
-  board.each do|turn|
-  if (turn=="X") ||(turn=="O")
-    count+=1 
-  else 
-    count+=0 
-  end
+  
 end
   
